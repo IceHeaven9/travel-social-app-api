993089bf0e926e750bc3441855be3a5a4843e72f
Cristhian Medrano Bonora <167735360+IceHeaven9@users.noreply.github.com>
2024-06-17 22:22:12 +0200

    firs commit

---
+++ src/database/data/initial-data.js
@@ -0,0 +1,13 @@
+import { hashPassword } from "../../utils/hash-password.js";
+import { db } from "../db.js";
+import { createUser } from "../users.js";
+
+export const adminId = await createUser({
+	name: "ADMIN",
+	email: "admin@travel-journal.com",
+	hashedPassword: await hashPassword("user1234"),
+	username: "admin",
+	role: "ADMIN",
+});
+
+await db.end();
---
+++ src/emails/email.js
@@ -0,0 +1,16 @@
+import nodemailer from "nodemailer";
+import {
+	EMAIL_HOST,
+	EMAIL_PASS,
+	EMAIL_PORT,
+	EMAIL_USER,
+} from "../constants.js";
+
+export const transport = nodemailer.createTransport({
+	host: EMAIL_HOST,
+	port: EMAIL_PORT,
+	auth: {
+		user: EMAIL_USER,
+		pass: EMAIL_PASS,
+	},
+});
